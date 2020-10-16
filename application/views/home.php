9e30e22e38a1d4b2cf2d4419cd65299fdf77fb02
MirHmzh <corn.row81@gmail.com>
2020-10-16 20:52:47 +0700

    update home

--- application/views/home.php
+++ application/views/home.php
@@ -5,6 +5,10 @@
 </head>
 <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/bootstrap.min.css') ?>">
 <style type="text/css">
+	@font-face {
+	  font-family: Lora;
+	  src: url(<?= base_url('assets/font/') ?>Lora.ttf);
+	}
 	body{
 		background-color: #313e28;
 		color: #313e28;
@@ -24,10 +28,10 @@
 	}
 	.menu-item{
 		text-align: center;
-		background-color: #e2e797;
+		background-color: #fff;
 		border-radius: 0.5em;
 		padding: 1em 0em;
-		margin: 0.5em 6em;
+		margin: 0.5em 3em;
 	}
 	a{
 		text-decoration: none;
@@ -50,64 +54,201 @@
 		text-decoration: none;
 		color: #313e28;
 	}
+	.brand{
+		position: fixed;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+	.brand-splash{
+		font-family: Lora;
+		color: #e2e797;
+		position: fixed;
+		top: 90%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+	.brand-footer{
+		font-family: Lora;
+		font-weight: 900;
+	}
+	.content{
+		/*display: none;*/
+	}
+	#cloud-left{
+		position: absolute;
+		transform: rotateX(180deg);
+	}
+	#cloud-right{
+		position: absolute;
+		transform: rotate(180deg);
+		right: 0%;
+	}
+	.logo-spacer{
+		color: #e2e797;
+		border: 1px solid #e2e797;
+		width: 40%;
+	}
+	.menu-wrapper{
+		background-color: #e2cab3;
+		position: fixed;
+		width: 101%;
+		left: -0.5%;
+		border-radius: 11% 11% 10% 10% / 11% 11% 0% 0%;
+		padding: 4em 0em;
+		/*display: none;*/
+	}
+	.menu-icon{
+		background-color: #313e28;
+		position: fixed;
+		padding: 0.8em 0.6em;
+		margin-top: -0.5em;
+		left: 10%;
+		border-radius: 0.3em;
+	}
+	.menu-icon img{
+		width: 2em;
+	}
+	.flower-wrapper{
+		opacity: 23%;
+		height: 3vh;
+	}
+	.flower-wrapper div img{
+		width: 30%;
+	}
+	.flower-right{
+		transform: rotate(180deg);
+		position: fixed;
+		right: 10%;
+	}
+	.flower-left{
+		transform: rotateX(180deg);
+		position: fixed;
+		left: 10%;
+	}
+	.menu-item-wrapper{
+		margin-top: 1em;
+	}
+	.footer-wrapper{
+		margin-top: 65vh;
+		padding: 1.9em 0em 0.3em 0em;
+		background-color: #fff;
+	}
+	.brand-footer span{
+		position: fixed;
+		bottom: 2em;
+	}
+	.compass-icon{
+		text-align: right;
+	}
+	.compass-icon img{
+		width: 30%;
+	}
 </style>
 <body>
-	<div class="row">
-		<div class="col-10">
-
+	<!-- <div class="splash">
+		<div class="brand">
+			<img src="<?= base_url('assets/img/logo.png') ?>">
 		</div>
-		<div class="col-2" id="compass-wrapper">
-			<a href="<?= base_url('kiblat/'.$token) ?>" title="">
-			<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" version="1.1" width="2em" height="2em" x="0" y="0" viewBox="0 0 128 128" style="enable-background:new 0 0 512 512" xml:space="preserve"><g><script/><script/><g xmlns="http://www.w3.org/2000/svg"><path d="m64 4.557a59.443 59.443 0 1 0 59.443 59.443 59.51 59.51 0 0 0 -59.443-59.443zm0 115.386a55.943 55.943 0 1 1 55.943-55.943 56.006 56.006 0 0 1 -55.943 55.943z" fill="#ffffff" data-original="#000000" style=""/><path d="m99.342 29.082a1.685 1.685 0 0 0 -.191-.234 1.735 1.735 0 0 0 -.232-.19 49.611 49.611 0 0 0 -69.838 0 1.735 1.735 0 0 0 -.232.19 1.685 1.685 0 0 0 -.191.234 49.609 49.609 0 0 0 0 69.836 1.685 1.685 0 0 0 .191.234 1.744 1.744 0 0 0 .233.19 49.609 49.609 0 0 0 69.836 0 1.744 1.744 0 0 0 .233-.19 1.685 1.685 0 0 0 .191-.234 49.609 49.609 0 0 0 0-69.836zm-66.742 68.788 3.3-3.3a1.75 1.75 0 1 0 -2.471-2.47l-3.3 3.3a46.1 46.1 0 0 1 0-62.79l3.3 3.3a1.75 1.75 0 0 0 2.471-2.482l-3.3-3.3a46.1 46.1 0 0 1 62.79 0l-3.3 3.3a1.75 1.75 0 1 0 2.481 2.472l3.3-3.3a46.1 46.1 0 0 1 0 62.79l-3.3-3.3a1.75 1.75 0 0 0 -2.471 2.482l3.3 3.3a46.1 46.1 0 0 1 -62.79 0z" fill="#ffffff" data-original="#000000" style=""/><path d="m106.026 67.07h-5.963v-1.95h5.414v-2.465h-5.414v-1.725h5.963v-2.465h-8.718v11.07h8.718z" fill="#ffffff" data-original="#000000" style=""/><path d="m34.745 65.378-2.289-6.913h-2.965l-2.303 6.913-1.693-6.913h-2.98l3.223 11.07h2.771l2.465-7.009 2.466 7.009h2.771l3.206-11.07h-2.98z" fill="#ffffff" data-original="#000000" style=""/><path d="m61.591 27.043 4.819 6.542h2.755v-11.07h-2.755v6.542l-4.819-6.542h-2.756v11.07h2.756z" fill="#ffffff" data-original="#000000" style=""/><path d="m64.685 98.556c-1.643-.354-2.176-.58-2.176-1.256 0-.548.5-.967 1.387-.967a5.307 5.307 0 0 1 3.158 1.176l1.4-1.966a6.692 6.692 0 0 0 -4.48-1.514c-2.529 0-4.238 1.418-4.238 3.544 0 2.224 1.757 2.869 4.125 3.4 1.611.387 2.127.532 2.127 1.193 0 .6-.532 1.015-1.563 1.015a5.657 5.657 0 0 1 -3.641-1.4l-1.547 1.853a7.493 7.493 0 0 0 5.092 1.853c2.739 0 4.43-1.386 4.43-3.609.002-2.078-1.592-2.79-4.074-3.322z" fill="#ffffff" data-original="#000000" style=""/><path d="m81.975 43.59-22.39 15.556a1.74 1.74 0 0 0 -.439.438l-15.556 22.39a1.75 1.75 0 0 0 2.435 2.436l22.39-15.556a1.74 1.74 0 0 0 .439-.438l15.556-22.39a1.75 1.75 0 0 0 -2.435-2.436zm-17.975 23.41a3 3 0 1 1 3-3 3 3 0 0 1 -3 3zm-5.753.014a6.535 6.535 0 0 0 2.74 2.74l-8.978 6.238zm11.506-6.026a6.535 6.535 0 0 0 -2.74-2.74l8.978-6.238z" fill="#ffffff" data-original="#000000" style=""/></g><script/></g></svg></a>
+		<div class="brand-splash">
+			alsujud.com
 		</div>
-	</div>
-	<div class="row justify-content-center">
-		<div class="col-4">
-
+	</div> -->
+	<div class="content">
+		<div id="cloud-left">
+			<img src="<?= base_url('assets/img/cloud.png') ?>" alt="">
 		</div>
-		<div class="col-4" id="logo-wrapper">
-			Logo belongs to here
+		<div id="cloud-right">
+			<img src="<?= base_url('assets/img/cloud.png') ?>" alt="">
 		</div>
-	</div>
-	<div class="menu-wrapper">
+		<br>
+		<br>
 		<div class="row justify-content-center">
-			<div class="col-12">
-				<a href="<?= base_url('bacaan_sholat/'.$token) ?>" title="">
-					<div class="menu-item">
-						Bacaan Sholat
-					</div>
-				</a>
+			<div class="col-12" id="logo-wrapper">
+				<img src="<?= base_url('assets/img/logo.png') ?>">
+				<hr class="logo-spacer">
 			</div>
 		</div>
-		<div class="row justify-content-center">
-			<div class="col-12">
-				<a href="<?= base_url('setelah_sholat/'.$token) ?>" title="">
-					<div class="menu-item">
-						Doa Setelah Sholat
+		<div class="flower-wrapper">
+			<div class="flower-left">
+				<img src="<?= base_url('assets/img/flower.png') ?>" alt="">
+			</div>
+			<div class="flower-right">
+				<img src="<?= base_url('assets/img/flower.png') ?>" alt="">
+			</div>
+			<div class="clear"></div>
+		</div>
+		<div class="menu-wrapper">
+			<div class="row justify-content-center menu-item-wrapper">
+				<div class="col-12">
+					<div class="menu-icon">
+						<img src="<?= base_url('assets/img/man.png') ?>">
 					</div>
-				</a>
+					<a href="<?= base_url('bacaan_sholat/'.$token) ?>" title="">
+						<div class="menu-item">
+							Bacaan Sholat
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="row justify-content-center menu-item-wrapper">
+				<div class="col-12">
+					<div class="menu-icon">
+						<img src="<?= base_url('assets/img/women.png') ?>">
+					</div>
+					<a href="<?= base_url('setelah_sholat/'.$token) ?>" title="">
+						<div class="menu-item">
+							Doa Setelah Sholat
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="row justify-content-center menu-item-wrapper">
+				<div class="col-12">
+					<div class="menu-icon">
+						<img src="<?= base_url('assets/img/marble_cream.png') ?>">
+					</div>
+					<a href="<?= base_url('dzikir_pilihan/'.$token) ?>" title="">
+						<div class="menu-item">
+							Dzikir Pilihan
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="row justify-content-center menu-item-wrapper">
+				<div class="col-12">
+					<div class="menu-icon">
+						<img src="<?= base_url('assets/img/holy_cream.png') ?>">
+					</div>
+					<a href="<?= base_url('juz_amma/'.$token) ?>" title="">
+						<div class="menu-item">
+							Juz Amma
+						</div>
+					</a>
+				</div>
 			</div>
 		</div>
-		<div class="row justify-content-center">
-			<div class="col-12">
-				<a href="<?= base_url('dzikir_pilihan/'.$token) ?>" title="">
-					<div class="menu-item">
-						Dzikir Pilihan
-					</div>
-				</a>
-			</div>
-		</div>
-		<div class="row justify-content-center">
-			<div class="col-12">
-				<a href="<?= base_url('juz_amma/'.$token) ?>" title="">
-					<div class="menu-item">
-						Juz Amma
-					</div>
-				</a>
+		<div class="footer-wrapper">
+			<div class="row">
+				<div class="col-6 brand-footer">
+					<span>alsujud.com</span>
+				</div>
+				<div class="col-6 compass-icon">
+					<span><img src="<?= base_url('assets/img/compass.png') ?>"></span>
+				</div>
 			</div>
 		</div>
 	</div>
 </body>
 <script src="<?= base_url('assets/js/jquery.min.js') ?>" type="text/javascript" charset="utf-8"></script>
 <script src="<?= base_url('assets/js/bootstrap.min.js') ?>" type="text/javascript" charset="utf-8"></script>
+<script type="text/javascript" charset="utf-8">
+	// $(document).ready(function() {
+	// 	setTimeout(() => {
+	// 		$('.splash').fadeOut(500, () => {
+	// 			$('.content').fadeIn(0);
+	// 		});
+	// 	}, 1000)
+	// });
+</script>
 </html>
